661932ed105acd74ed28cbfc06605184ea600de3
Javohir Elmurodov <john.lgd65@gmail.com>
2022-02-17 18:27:23 +0500

    basic

--- src/main/java/uz/elmurodov/spring_security/config/security/SecurityConfigurer.java
+++ src/main/java/uz/elmurodov/spring_security/config/security/SecurityConfigurer.java
@@ -2,6 +2,8 @@ package uz.elmurodov.spring_security.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +13,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static uz.elmurodov.spring_security.enums.Role.ADMIN;
 import static uz.elmurodov.spring_security.enums.Role.MANAGER;
@@ -25,6 +30,10 @@ import static uz.elmurodov.spring_security.enums.Role.MANAGER;
 )
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
+    public static final String[] WHITE_LIST = {
+            "/", "/auth/login"
+    };
+
     private final PasswordEncoder passwordEncoder;
 
     public SecurityConfigurer(PasswordEncoder passwordEncoder) {
@@ -35,17 +44,34 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-//                .antMatchers("/secured-role-admin").hasRole("ADMIN")
-//                .antMatchers("/secured-role-manager").hasAnyRole("ADMIN", "MANAGER")
-//                .antMatchers("/secured-delete-manager").hasAuthority("manager_delete")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authorizeRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry
+                                .antMatchers(WHITE_LIST).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin(httpSecurityFormLoginConfigurer ->
+                        httpSecurityFormLoginConfigurer
+                                .loginPage("/auth/login")
+                                .defaultSuccessUrl("/secured", false)
+                                .loginProcessingUrl("/auth/login")
+                )
+                .rememberMe(httpSecurityRememberMeConfigurer -> {
+                    httpSecurityRememberMeConfigurer
+                            .key("!@#!@#W#ERFGSD$T##$%^%^$%$^%^@#$@#$WERREFG$#%")
+                            .rememberMeParameter("remember-me")
+                            .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(40));
+                })
+                .logout(httpSecurityLogoutConfigurer ->
+                        httpSecurityLogoutConfigurer
+                                .logoutUrl("/auth/logout")
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .deleteCookies("JSESSIONID", "remember-me")
+                );
+        ;
     }
-
 
     @Bean
     @Override
